c7c51b654b35ab928eb0df392ea064905d34d4b0
Fenixssj <ale.lagosp@duocuc.cl>
2024-09-05 19:51:01 -0400

    segundo push

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -10,15 +10,42 @@ import { AlertController } from '@ionic/angular';
 export class HomePage {
 
   username: string = 'guest';
+  name!: string;
+  lastname!: string;
+  edLevel!: string;
+  birthday!: string;
+
+  edLevels: Map<string, string> = new Map<string, string>
 
   constructor(
     private router: Router,
     private alertController: AlertController
   ) {
-    const state = this.router.getCurrentNavigation()?.extras?.state;
-    if (state && state['user']) {
-      console.log(`User: ${state['user']}`);
+    let state = this.router.getCurrentNavigation()?.extras?.state;
+    if (state) {
+      console.log(`User: ${state['user']}`)
       this.username = state['user'];
     }
+
+    this.edLevels.set('pre', 'Pre Basica');
+    this.edLevels.set('basic', 'Ed Basica');
+    this.edLevels.set('medium', 'Ed Media');
+    this.edLevels.set('superior', 'Ed Superior');
+    this.edLevels.set('post', 'Postgrado');
+  }
+
+  async showInfo() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: `Datos del usuario: ${this.name} ${this.lastname}`
+    });
+    alert.present();
+  }
+
+  clean(){
+    this.birthday = '';
+    this.name = '';
+    this.lastname = '';
+    this.edLevel = '';
   }
 }
